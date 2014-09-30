60c1f53cd6fc0ab4be4bdffbca1eb9ee913cae2c
Nat Gunther Roth <nroth@corn17.stanford.edu>
2014-09-29 19:07:29 -0700

    adding delta test2

--- pa1/java/src/cs224n/wordaligner/IBMM1.java
+++ pa1/java/src/cs224n/wordaligner/IBMM1.java
@@ -33,8 +33,8 @@ public class IBMM1 implements WordAligner {
   }
 
   
-  private int getDelta(int i, int j,int k){
-	  int numerator = t.getCount(, value)
+  private int getDelta(String sourceWord, String targetWord,int numTargetWords){
+	  double numerator = t.getCount(sourceWord, targetWord);
 	  return 1; 
   }
   
