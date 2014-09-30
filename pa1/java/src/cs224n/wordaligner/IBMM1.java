cf045c9ddd7527a78eeb1e71ab644c0a7eae8e4f
Brandon Taylor Ewonus <bewonus@corn18.stanford.edu>
2014-09-30 14:35:51 -0700

    minor edits

--- pa1/java/src/cs224n/wordaligner/IBMM1.java
+++ pa1/java/src/cs224n/wordaligner/IBMM1.java
@@ -4,7 +4,7 @@ import cs224n.util.*;
 import java.util.List;
 
 /**
- * A word alignment model
+ * A word alignment model based on IBM Model 1.
  *
  * @author Brandon Ewonus
  * @author Nat Roth
@@ -13,43 +13,56 @@ public class IBMM1 implements WordAligner {
 
   private static final long serialVersionUID = 1315751943476440515L;
 
-  // TODO: Use arrays or Counters for collecting sufficient statistics
-  // from the training data.
+  /** Counters for collecting sufficient statistics from the training data. */
   private CounterMap<String, String> countTargetSource; // c(e_j^(k), f_i^(k))
-  private Counter<String> countTarget; // c(e_j^(k))
-  private CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> countPosition; // c(j|i,l,m)
-  private CounterMap<Integer, Integer> countTargetLengthSourceLength; // c(i,l,m)
+  private Counter<String> countSource; // c(f_i^(k))
+  private CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> countPosition; // c(j|i,n,m)
+  private CounterMap<Integer, Integer> countTargetLengthSourceLength; // c(i,n,m)
 
-  // model paramaters to estimate
-  private CounterMap<String, String> t; // t(f|e)
-  private CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> q; // q(j|i,l,m)
+  /** Model parameters to estimate. */
+  private CounterMap<String, String> t; // t(e|f)
+  private CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> q; // q(j|i,n,m)
 
-
+  /**
+   * Uses the IBMM1 EM inference algorithm to predict alignments
+   * based on the sufficient statistics collected from train().
+   *
+   * @param sentencePair The sentence pair to align.
+   * @return alignment
+   */
   public Alignment align(SentencePair sentencePair) {
-    // Placeholder code below.
-    // TODO Implement an inference algorithm for Eq.1 in the assignment
-    // handout to predict alignments based on the counts you collected with train().
     Alignment alignment = new Alignment();
     return alignment;
   }
 
-  
-  private int getDelta(String sourceWord, String targetWord,int numTargetWords){
+  /**
+   * Return delta. // TODO: compute deltas
+   *
+   * @param sourceWord
+   * @param targetWord
+   * @param numTargetWords
+   * @return delta
+   */
+  private double getDelta(String sourceWord, String targetWord, int numTargetWords){
 	  double numerator = t.getCount(sourceWord, targetWord);
-	  return 1; 
+	  return 1.0;
   }
   
   public void train(List<SentencePair> trainingPairs) {
 
+    t = new CounterMap<String, String>();
+
     for (SentencePair pair : trainingPairs) {
-      // TODO: initialize deltas (for first iteration); requires prior initialization of t parameters
+      // TODO: initialize deltas (for first iteration); based on uniform initialization of t parameters
+
     }
 
+    // TODO: determine better convergence criteria
     int T = 10; // max iterations, equals 10 for now
-    for (int t = 0; t < T; t++) {
+    for (int iteration = 0; iteration < T; iteration++) {
       // set all counts c to zero
       countTargetSource = new CounterMap<String, String>();
-      countTarget = new Counter<String>();
+      countSource = new Counter<String>();
       countPosition = new CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>>();
       countTargetLengthSourceLength = new CounterMap<Integer, Integer>();
 
@@ -63,9 +76,9 @@ public class IBMM1 implements WordAligner {
         int numTargetWords = targetWords.size();
         for (int i = 0; i < numSourceWords; i++) { // loop through source words
           for (int j = 0; j < numTargetWords; j++) { // loop through target words
-            int delta = getDelta(sourceWords.get(i),targetWords.get(j),numTargetWords); // TODO: use actual delta
+            double delta = getDelta(sourceWords.get(i), targetWords.get(j), numTargetWords);
             countTargetSource.incrementCount(targetWords.get(j), sourceWords.get(i), delta);
-            countTarget.incrementCount(targetWords.get(j), delta);
+            countSource.incrementCount(targetWords.get(j), delta);
             Pair<Integer, Integer> positionPair = new Pair<Integer, Integer>(j, i);
             Pair<Integer, Integer> lengthsPair = new Pair<Integer, Integer>(numTargetWords, numSourceWords);
             countPosition.incrementCount(positionPair, lengthsPair, delta);
@@ -74,8 +87,20 @@ public class IBMM1 implements WordAligner {
         }
       }
 
-      // TODO: update t (and q?), then deltas
+      // TODO: update t parameters
+      for (String target : countTargetSource.keySet()) { // set of target words (english)
+        for (String source : countTargetSource.getCounter(target).keySet()) { // set of source words corresponding to current target (french)
+          double updatedValue = countTargetSource.getCount(target, source) / countSource.getCount(source);
+          t.setCount(target, source, updatedValue);
+        }
+      }
+
+      // TODO: update deltas based on new t
 
     }
+
+    // TODO: set q parameters
+    q = new CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>>();
+
   }
 }
--- pa1/java/src/cs224n/wordaligner/PMIModel.java
+++ pa1/java/src/cs224n/wordaligner/PMIModel.java
@@ -13,67 +13,78 @@ public class PMIModel implements WordAligner {
 
   private static final long serialVersionUID = 1315751943476440515L;
 
-  // TODO: Use arrays or Counters for collecting sufficient statistics
-  // from the training data.
+  /** Counters for collecting sufficient statistics from the training data. */
   private double totalSentences;
   private Counter<String> sourceCounts;
   private Counter<String> targetCounts;
   private CounterMap<String,String> sourceTargetCounts;
 
+  /**
+   * Uses the inference algorithm for Eq.1 in the assignment handout
+   * to predict alignments based on the counts collected from train().
+   *
+   * @param sentencePair The sentence pair to align.
+   * @return alignment
+   */
   public Alignment align(SentencePair sentencePair) {
-    // Placeholder code below.
-    // TODO Implement an inference algorithm for Eq.1 in the assignment
-    // handout to predict alignments based on the counts you collected with train().
     Alignment alignment = new Alignment();
     List<String> sourceWords = sentencePair.getSourceWords();
     List<String> targetWords = sentencePair.getTargetWords();
     int numSourceWords = sourceWords.size();
     int numTargetWords = targetWords.size();
 
-
-    sourceWords.add(null);
-
     for (int tgtIndex = 0; tgtIndex < numTargetWords; tgtIndex++) {
+      // Alignment a_i is initially set to -1 (corresponding to null alignment)
       int a_i = -1;
-//      double maxPMI = 1.0/totalSentences; // double maxPMI = 0.0;
-      double maxPMI = 0.0;
+      // We always have c(null, e_i) / (c(null), c(e_i)) == 1.0/totalSentences
+      double maxPMI = 1.0/totalSentences;
       double currentPMI;
-      for (int srcIndex = 0; srcIndex < numSourceWords + 1; srcIndex++) {
+      for (int srcIndex = 0; srcIndex < numSourceWords; srcIndex++) {
         currentPMI = sourceTargetCounts.getCount(sourceWords.get(srcIndex), targetWords.get(tgtIndex)) /
           (sourceCounts.getCount(sourceWords.get(srcIndex)) * targetCounts.getCount(targetWords.get(tgtIndex)));
-        System.out.println("joint count " + sourceTargetCounts.getCount(sourceWords.get(srcIndex), targetWords.get(tgtIndex)));
-        System.out.println("source count " + sourceCounts.getCount(sourceWords.get(srcIndex)));
-        System.out.println("target count " + targetCounts.getCount(targetWords.get(tgtIndex)));
-        System.out.println("PMI: " + sourceWords.get(srcIndex) + " " + targetWords.get(tgtIndex) + currentPMI + "\n");
+
+// sanity check!
+//        System.out.println("joint count " + sourceTargetCounts.getCount(sourceWords.get(srcIndex), targetWords.get(tgtIndex)));
+//        System.out.println("source count " + sourceCounts.getCount(sourceWords.get(srcIndex)));
+//        System.out.println("target count " + targetCounts.getCount(targetWords.get(tgtIndex)));
+//        System.out.println("PMI: " + sourceWords.get(srcIndex) + " " + targetWords.get(tgtIndex) + currentPMI + "\n");
+
+        // Update a_i and maxPMI
         if (currentPMI > maxPMI) { // TODO: tiebreakers?
           a_i = srcIndex;
           maxPMI = currentPMI;
         }
       }
-      if (sourceWords.get(a_i) != null) {
-//      if (a_i > -1) {
+      // If a_i > -1, then e_i is not aligned to null, so we add the predicted alignment
+      if (a_i > -1) {
         alignment.addPredictedAlignment(tgtIndex, a_i);
       }
     }
     return alignment;
   }
 
+  /**
+   * Collects sufficient statistics from the training data.
+   *
+   * @param trainingPairs
+   */
   public void train(List<SentencePair> trainingPairs) {
     totalSentences = trainingPairs.size();
     sourceCounts = new Counter<String>();
     targetCounts = new Counter<String>();
     sourceTargetCounts = new CounterMap<String,String>();
+
+    // Loop through training pairs
     for(SentencePair pair : trainingPairs){
       List<String> targetWords = pair.getTargetWords();
       List<String> sourceWords = pair.getSourceWords();
-//      if (sourceWords.size() < targetWords.size()) {
-        sourceWords.add(null);
-//      }
+
+      // Need to add target word counts exactly once
       boolean allTargetsAdded = false;
+
       for(String source : sourceWords) {
         sourceCounts.incrementCount(source, 1.0);
         for(String target : targetWords){
-          // TODO: Warm-up. Your code here for collecting sufficient statistics.
           if (!allTargetsAdded) {
             targetCounts.incrementCount(target, 1.0);
           }
@@ -81,10 +92,6 @@ public class PMIModel implements WordAligner {
         }
         allTargetsAdded = true;
       }
-//      for(String target : targetWords) {
-//        targetCounts.incrementCount(target, 1.0);
-//      }
-      sourceWords.remove(sourceWords.size() - 1);
     }
   }
 }
