7258c5c39f108375a6884d20b0530cc9533e13e2
Nat Gunther Roth <nroth@corn17.stanford.edu>
2014-09-30 17:30:06 -0700

    broken IBM1

--- pa1/java/src/cs224n/wordaligner/IBMM1.java
+++ pa1/java/src/cs224n/wordaligner/IBMM1.java
@@ -1,7 +1,10 @@
 package cs224n.wordaligner;
 
 import cs224n.util.*;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * A word alignment model based on IBM Model 1.
@@ -16,12 +19,12 @@ public class IBMM1 implements WordAligner {
   /** Counters for collecting sufficient statistics from the training data. */
   private CounterMap<String, String> countTargetSource; // c(e_j^(k), f_i^(k))
   private Counter<String> countSource; // c(f_i^(k))
-  private CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> countPosition; // c(j|i,n,m)
-  private CounterMap<Integer, Integer> countTargetLengthSourceLength; // c(i,n,m)
+  private CounterMap<Integer,Pair<Integer, Pair<Integer, Integer>>> countPosition; // c(j|i,n,m)
+  //private CounterMap<Integer,Pair<Integer,Integer>> countTargetLengthSourceLength; // c(i,n,m)
 
   /** Model parameters to estimate. */
   private CounterMap<String, String> t; // t(e|f)
-  private CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>> q; // q(j|i,n,m)
+  private CounterMap<Integer,Pair<Integer, Pair<Integer, Integer>>> q; // q(j|i,n,m)
 
   /**
    * Uses the IBMM1 EM inference algorithm to predict alignments
@@ -31,7 +34,27 @@ public class IBMM1 implements WordAligner {
    * @return alignment
    */
   public Alignment align(SentencePair sentencePair) {
+	  
     Alignment alignment = new Alignment();
+    List<String> targetWords = sentencePair.getTargetWords();
+    List<String> sourceWords = sentencePair.getSourceWords();
+    sourceWords.add(null);
+    for(int i = 0; i < targetWords.size();i++){
+    	double maxMatch = 0.0;
+    	int a_i = -1;
+    	for(int j = 0; j < sourceWords.size();j++){
+    		double probMatch = t.getCount(sourceWords.get(j),targetWords.get(i));
+    		if(probMatch > maxMatch){
+    			maxMatch = probMatch;
+    			a_i = j;
+    		}
+    	}
+    	//if alignment wasn't with null.
+    	if(sourceWords.get(a_i) != null){
+    		alignment.addPredictedAlignment(i, a_i);
+    	}
+    }
+    sourceWords.remove(null);    
     return alignment;
   }
 
@@ -43,64 +66,100 @@ public class IBMM1 implements WordAligner {
    * @param numTargetWords
    * @return delta
    */
-  private double getDelta(String sourceWord, String targetWord, int numTargetWords){
-	  double numerator = t.getCount(sourceWord, targetWord);
-	  return 1.0;
+  private double getDelta(String sourceWord, String targetWord, List<String> sourceList,List<String> targetList ){
+	  double numerator = t.getCount(targetWord,sourceWord);
+	  double denominator = 0.0;
+	  
+	  for(int i = 0; i < sourceList.size();i++){
+		  denominator += t.getCount(sourceList.get(i),targetWord);
+	  }
+	  //System.out.print("---"+denominator);
+	  
+	  return numerator/denominator;
+  }
+  
+  private void initializeT(Set<String> sourceSet,Set<String> targetSet){
+	  double uniformPrior = 1.0 / targetSet.size(); 
+	  for(String targetWord: targetSet){
+		  for(String sourceWord: sourceSet){
+			  t.setCount(sourceWord,targetWord,uniformPrior);
+		  }
+	  }
+  }
+  
+  Set<String> getCorpus(List<SentencePair> pairs,boolean getTarget){
+	  Set<String> corpus = new HashSet<String>();
+	  for(int i = 0;i<pairs.size();i++){
+		  List<String> toAdd;
+		  if(getTarget){
+			  toAdd = pairs.get(i).getTargetWords();
+		  }else{
+			  toAdd = pairs.get(i).getSourceWords();
+		  }
+		  corpus.addAll(toAdd);
+	  }
+	  //add null to source corpus, but not target corpus.
+	  if(!getTarget) corpus.add(null);
+	  return corpus;
   }
   
   public void train(List<SentencePair> trainingPairs) {
 
     t = new CounterMap<String, String>();
-
-    for (SentencePair pair : trainingPairs) {
-      // TODO: initialize deltas (for first iteration); based on uniform initialization of t parameters
-
-    }
+    initializeT(getCorpus(trainingPairs,false),getCorpus(trainingPairs,true));
+   
 
     // TODO: determine better convergence criteria
-    int T = 10; // max iterations, equals 10 for now
+    int T = 100; // max iterations, equals 10 for now
     for (int iteration = 0; iteration < T; iteration++) {
       // set all counts c to zero
       countTargetSource = new CounterMap<String, String>();
       countSource = new Counter<String>();
-      countPosition = new CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>>();
-      countTargetLengthSourceLength = new CounterMap<Integer, Integer>();
+      countPosition = new CounterMap<Integer,Pair<Integer, Pair<Integer, Integer>>>();
+      //countTargetLengthSourceLength = new CounterMap<Integer, Integer>();
 
 
       // loop through all sentence pairs
       int numSentencePairs = trainingPairs.size();
       for (int k = 0; k < numSentencePairs; k++) {
         List<String> sourceWords = trainingPairs.get(k).getSourceWords();
+        //add null 'word'
+        sourceWords.add(null);
+
         List<String> targetWords = trainingPairs.get(k).getTargetWords();
         int numSourceWords = sourceWords.size();
         int numTargetWords = targetWords.size();
         for (int i = 0; i < numSourceWords; i++) { // loop through source words
           for (int j = 0; j < numTargetWords; j++) { // loop through target words
-            double delta = getDelta(sourceWords.get(i), targetWords.get(j), numTargetWords);
-            countTargetSource.incrementCount(targetWords.get(j), sourceWords.get(i), delta);
-            countSource.incrementCount(targetWords.get(j), delta);
-            Pair<Integer, Integer> positionPair = new Pair<Integer, Integer>(j, i);
-            Pair<Integer, Integer> lengthsPair = new Pair<Integer, Integer>(numTargetWords, numSourceWords);
-            countPosition.incrementCount(positionPair, lengthsPair, delta);
-            countTargetLengthSourceLength.incrementCount(numTargetWords, numSourceWords, delta);
+            double delta = getDelta(sourceWords.get(i), targetWords.get(j), sourceWords,targetWords);
+            //System.out.print(""+delta);
+            countTargetSource.incrementCount(sourceWords.get(i),targetWords.get(j), delta);
+            countSource.incrementCount(sourceWords.get(i), delta);
+            Pair<Integer,Pair<Integer,Integer>> sourceLengthPair = new Pair<Integer,Pair<Integer,Integer>>(i,new Pair<Integer,Integer>(numSourceWords,numTargetWords));
+            countPosition.incrementCount(j, sourceLengthPair, delta);
+            //countTargetLengthSourceLength.incrementCount(numTargetWords, numSourceWords, delta);
           }
         }
+        //remove null word
+        sourceWords.remove(sourceWords.size()-1);
       }
-
-      // TODO: update t parameters
-      for (String target : countTargetSource.keySet()) { // set of target words (english)
-        for (String source : countTargetSource.getCounter(target).keySet()) { // set of source words corresponding to current target (french)
-          double updatedValue = countTargetSource.getCount(target, source) / countSource.getCount(source);
-          t.setCount(target, source, updatedValue);
+      
+      for(String key:t.keySet()){
+      	  Counter<String> valCounter = t.getCounter(key);
+      	  for(String val:valCounter.keySet()){
+      		  System.out.println("================================");
+      		  System.out.println(key);
+      		  System.out.println(val);
+      		  System.out.println(""+t.getCount(key, val));
+      	  }
         }
-      }
-
-      // TODO: update deltas based on new t
+      //Normalize t(e|f) setting it equal to c(e,f)/c(f)
+      t = Counters.conditionalNormalize(countTargetSource);
 
     }
+    
 
-    // TODO: set q parameters
-    q = new CounterMap<Pair<Integer, Integer>, Pair<Integer, Integer>>();
-
+    // TODO: set q parameters -- once at end, conditional normalization of c(j|i,l,m)
+    q = Counters.conditionalNormalize(countPosition);
   }
 }
